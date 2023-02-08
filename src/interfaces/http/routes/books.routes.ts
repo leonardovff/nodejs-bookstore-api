e37608cc14c6259a26a41f64328f59aee1fcacbe
Leonardo Victor <leonardovff@gmail.com>
2023-02-08 04:32:19 -0300

    Add initial book controller

--- src/interfaces/http/routes/books.routes.ts
+++ src/interfaces/http/routes/books.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import { getBooks } from '../../../controllers/books.controller';
 
 const booksRoutes = express.Router();
+booksRoutes.get('/', getBooks);
 
 export default booksRoutes;
