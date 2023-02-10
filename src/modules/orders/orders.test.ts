755e0bd761c838b4152ddf6c7f1b03470b8c1e65
Leonardo Victor <leonardovff@gmail.com>
2023-02-10 19:13:45 -0300

    Fix unit test for order entity

--- src/modules/orders/orders.test.ts
+++ src/modules/orders/orders.test.ts
@@ -1,21 +1,42 @@
 import { randomUUID } from 'crypto';
-import { calculateTotalPriceCentsForOneOrder } from './orders';
+import Orders from './orders';
 
-describe('Orders entity', () => {
-  test('should calculate correctly the total price for all books in one order', () => {
+describe.only('Orders entity - create', () => {
+  test('should create a order when there is just one book', () => {
+    const bookId1 = randomUUID();
+    const userId = randomUUID();
+    const booksPrices = {
+      [bookId1]: 2020,
+    };
+
+    const { order } = Orders.create({
+      booksIds: [bookId1],
+      booksPrices,
+      userId,
+    });
+
+    expect(order).toBeTruthy();
+    expect(order).toMatchObject({
+      userId,
+      bookIds: [bookId1],
+      totalPriceCents: 2020,
+    });
+  });
+  test('should create a create a order and calculate correctly the total price for all books in the order when there is more than one book', () => {
     const bookId1 = randomUUID();
     const bookId2 = randomUUID();
+    const userId = randomUUID();
     const booksPrices = {
       [bookId1]: 2020,
       [bookId2]: 3123,
     };
 
-    const priceCents = calculateTotalPriceCentsForOneOrder(
-      [bookId1, bookId2, bookId1],
-      booksPrices
-    );
+    const { order: { totalPriceCents } } = Orders.create({
+      booksIds: [bookId1, bookId2, bookId1],
+      booksPrices,
+      userId,
+    });
 
-    expect(priceCents).toEqual(7163);
+    expect(totalPriceCents).toEqual(7163);
   });
 });
-
